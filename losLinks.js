eb8042aabccdb0019ac895a5439d57c5dd097e82
iohehe <ioeheh@yeah.net>
2020-11-27 20:13:24 +0800

    let _los=1 show all

--- losLinks.js
+++ losLinks.js
@@ -3,9 +3,9 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      iohex
-// @description 2020/11/27下午6:39:10
+// @description target to javascript:you know
 // ==/UserScript==
 
 //在url中加入_los参数触发
@@ -34,20 +34,26 @@ function getQueryVariable(target_var) {
 }
 
 // 从href中拿关键信息
-//todo: 检查每个href=(*)处
-function losLinks(key_word)
-{
+function losLinks(key_word) {
   console.log("=============> lose links ==========>");
-  //todo: 不只有a标签有href属性
+  console.log("key_word: "+key_word);
   let _innerHtml = document.getElementsByTagName('a');
   for (let i=0; i<_innerHtml.length; i++)
   {
     let _link = _innerHtml[i].href;
-    let _s = _link.indexOf(key_word);
-    //todo: 过滤_los参数自己本身带有key_word的链接
-    if (_s != -1)
+    
+    //_loc=1 输出所有link
+    if (key_word == '1')
     {
       console.log(_link);
+    }
+    else
+    {
+      let _s = _link.indexOf(key_word);
+      if (_s != -1)
+      {
+        console.log(_link);
+      }
     }
   }
 }
